d0f01dccb2e2b390d6e88fb0ef84f152ee61b29b
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2021-11-22 12:33:37 -0500

    Create Character.java

---
+++ v1/Character.java
@@ -0,0 +1,22 @@
+public class Character {
+
+  public int HP;
+  public int strength;
+  public int defense;
+  public double attack;
+  
+  public boolean isAlive() {
+  
+  }
+  
+  public int getDefense() {
+  
+  }
+  
+  public int lowerHP(int dmg) {
+  
+  }
+  
+  public int attack
+
+}
