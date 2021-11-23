e4ca413670bec2c53df175a67f87d6b93943d733
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2021-11-22 19:41:28 -0500

    Update Monster.java

--- v0/Monster.java
+++ v0/Monster.java
@@ -32,31 +32,7 @@ public class Monster {
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-  /**public class Monster{
-  public Monster() {}
-
-  public boolean isAlive(){
-    int hp = 0;
-    if (hp > 0){
-      return true;
-    }
-    else {return false;}
-  }
-
-  public void specialize() {
-    atk = 5; // change later
-  }
-
-  public void normalize() {
-    atk = 2 // change later
-  }
-
-  public int attack(Protagonist targert){
-    return atk;
-  }
-
-}
-
+  /**
      boolean isAlive() -- tell whether I am alive
      post: returns boolean indicated alive or dead
   **/
