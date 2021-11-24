7b2a7cc6144375d77b326e17b1787ccb9cfd696b
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2021-11-23 22:58:23 -0500

    Create Tank.java

---
+++ v3/Tank.java
@@ -0,0 +1,26 @@
+public class Tank{
+
+  //stats initialization
+  public Tank(){
+    
+  }
+  
+  public Tank(String name) {
+    this();
+    _name = name;
+  }
+  
+  public static String about(){
+    return "High HP, high defense, low attack.";
+  }
+  
+  public void specialize(){
+  
+  }
+  
+  public void normalize(){
+  
+  }
+  
+
+}
