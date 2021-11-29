955d388dca6ac98897e0a4b4c09440a9b09c2882
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2021-11-28 19:52:50 -0500

    Update Goblin.java

--- v4/Goblin.java
+++ v4/Goblin.java
@@ -2,26 +2,15 @@ public class Goblin extends Monster {
 
   //stats initialization
   public Goblin(){
-    _hitPts = 70;
+    _hitPts = 50;
     _strength = 60;
-    _defense = 20;
+    _defense = 15;
     _attack = .7;
   }
 
   public static String about(){
     return "Low HP, low defense, high damage.";
   }
-  
-  public void specialize(){
-    _attack = .8;
-    _defense = 10;
-  }
-  
-  public void normalize(){
-    _attack = .6;
-    _defense = 10;
-  }
-  
 
 }
 
